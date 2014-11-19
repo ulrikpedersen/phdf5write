a4e955b93f6cadc7dc681260d2a1ae1e26a45268
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2014-11-19 11:10:00 +0000

    testing tweaks for mpi

--- libsrc/ndarray_hdf5.cpp
+++ libsrc/ndarray_hdf5.cpp
@@ -238,6 +238,10 @@ int NDArrayToHDF5::h5_open(const char *filename)
     	LOG4CXX_ERROR(log, "Failed to create file layout");
     }
 
+#ifdef H5_HAVE_PARALLEL
+    MPI_Barrier(this->mpi_comm);
+#endif
+
     return retcode;
 }
 
@@ -297,6 +301,10 @@ int NDArrayToHDF5::h5_close()
 //    	this->h5file = H5I_INVALID_HID;
 //    	retcode = -1;
 //    }
+
+#ifdef H5_HAVE_PARALLEL
+    MPI_Barrier(this->mpi_comm);
+#endif
 
     if (this->h5file != H5I_INVALID_HID) {
     	this->write_ndattributes();
